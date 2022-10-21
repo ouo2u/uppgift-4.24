11c0ce94ef78b38f13ed610f9af5ee706f28e6dc
Mopa01 <mopa05001@utb.vaxjo.se>
2022-10-21 10:17:46 +0200

    Add project files.

---
+++ uppgift 4.24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace upggift4._24
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            for (int i = 0; i <= 100; i++) 
+            { 
+            Console.WriteLine(i);
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+
+
+
+            }
+        }
+       
+    }
+}
